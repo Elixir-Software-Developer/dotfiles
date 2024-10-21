b82daa56e7f80778320e88c61195796f4a85fc38
Roberto Flores <fq211776@alumno.udb.edu.sv>
2024-10-21 11:30:46 -0600

    mas limpieza

--- fd/setup.sh
+++ fd/setup.sh
@@ -11,5 +11,8 @@ SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) && cd "$SDIR" || exit 1
 cargar_script "common_functions.sh"
 # cargar_script "backup_folders.sh"
 cargar_script "backup_files_config.sh"
+cargar_script "logo.sh"
 
+
+logo "Welc!"
 putisima pisona.sh
